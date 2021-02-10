f4ed46f4df91fbe260a68f4c41f11ccdf2dba686
estebam <estebam@ESTETIENNE>
2021-02-10 01:03:53 +0100

    Secondo Salvatagggio

--- src/main/java/com/example/accessingdatamysqlnote/MainController.java
+++ src/main/java/com/example/accessingdatamysqlnote/MainController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller // This means that this class is a Controller
+@Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
    
